c520b7b8ce321a10f6939c48d0abd953122f46b8
JoseCardona0 <jcardona@IT05>
2016-09-27 15:52:23 -0400

    Add project files.

---
+++ Print3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Print3.Startup))]
+namespace Print3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
